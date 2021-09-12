a5dc20b9fb9ce12d0e8b3d881e824ef664454319
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-12 15:04:04 +0200

    Declared functions from lab lecture

--- ADC_driver.h
+++ ADC_driver.h
@@ -9,6 +9,14 @@
 #ifndef ADC_DRIVER_H_
 #define ADC_DRIVER_H_
 
+void adc_init(void); 
+
+volatile uint8_t adc_read(uint8_t channel);
+
+void adc_calibrate(void); //should find the returned adc values at the extrems of the x and y axis
+//these returned adc values should then be used in pos_read for caluculating
+pos_t pos_read(void); //and returning the joystick state on a more suitable scale (ex: 1-100 for each axis) 
+
 
 
 #endif /* ADC_DRIVER_H_ */
