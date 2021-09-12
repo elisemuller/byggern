a1cd20b8cf783465726cd2410457f4d7fd68c2d5
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-12 15:54:42 +0200

    Update ADC_driver.h

--- ADC_driver.h
+++ ADC_driver.h
@@ -14,6 +14,8 @@ void adc_init(void);
 volatile uint8_t adc_read(uint8_t channel);
 
 void adc_calibrate(void); //should find the returned adc values at the extrems of the x and y axis
+
+
 //these returned adc values should then be used in pos_read for caluculating
 pos_t pos_read(void); //and returning the joystick state on a more suitable scale (ex: 1-100 for each axis) 
 
