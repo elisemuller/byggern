181be46137a6d55950cb11f317795cafb352c58b
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-17 15:51:38 +0200

    Update xmem.h

--- xmem.h
+++ xmem.h
@@ -26,6 +26,8 @@ void xmem_init(void);
 */
 void mcu_porta_io_config(uint8_t mcu_in);
 
+// void xmem_write(uint8_t data, uint16_t offset); forslag - definer base_address i ext_mem.c
+
 void xmem_write(uint8_t data, uint16_t address, uint16_t base);
 
 uint8_t xmem_read(uint16_t address, uint16_t base);
