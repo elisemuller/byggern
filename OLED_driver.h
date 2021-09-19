dd1d8e0c4e996a21978e0eabcd736c6a91df079d
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-19 16:51:06 +0200

    Declared OLED functions from lab lecture

--- OLED_driver.h
+++ OLED_driver.h
@@ -11,6 +11,23 @@
 
 #define OFFSET_OLED 0x0000
 
+void OLED_init(void);
+
+void OLED_reset(void);
+
+void OLED_home(void);
+
+void OLED_goto_line(int line);
+
+void OLED_clear_line(int line);
+
+void OLED_pos(int row, int column);
+
+void OLED_write_data(volatile char data); //volatile
+
+void OLED_print(char* data);
+
+void OLED_set_brightness(int lvl);
 
 
 
