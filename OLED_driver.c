2def86acf11a6f569fadd57bc646b7661105ccb1
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-19 17:58:07 +0200

    Update OLED_driver.c

--- OLED_driver.c
+++ OLED_driver.c
@@ -10,11 +10,39 @@
 #include "OLED_driver.h"
 #include "xmem.h"
 
+
+void OLED_init(void){
+ OLED_write_c(0xae); // display off
+ OLED_write_c(0xa1); //segment remap
+ OLED_write_c(0xda); //common pads hardware: alternative
+ OLED_write_c(0x12);
+ OLED_write_c(0xc8); //common output scan direction:com63~com0
+ OLED_write_c(0xa8); //multiplex ration mode:63
+ OLED_write_c(0x3f);
+ OLED_write_c(0xd5); //display divide ratio/osc. freq. mode
+ OLED_write_c(0x80);
+ OLED_write_c(0x81); //contrast control
+ OLED_write_c(0x50);
+ OLED_write_c(0xd9); //set pre-charge period
+ OLED_write_c(0x21);
+ OLED_write_c(0x20); //Set Memory Addressing Mode
+ OLED_write_c(0x02);
+ OLED_write_c(0xdb); //VCOM deselect level mode
+ OLED_write_c(0x30);
+ OLED_write_c(0xad); //master configuration
+ OLED_write_c(0x00);
+ OLED_write_c(0xa4); //out follows RAM content
+ OLED_write_c(0xa6); //set normal display
+ OLED_write_c(0xaf); // display on
+}
+
+
+
 void OLED_print_arrow(uint8_t row, uint8_t col){
   OLED_pos(row, col); 
-  OLED_write_data(0b00011000);
-  OLED_write_data(0b00011000);
-  OLED_write_data(0b01111110);
-  OLED_write_data(0b00111100);
-  OLED_write_data(0b00011000);
+  OLED_write_d(0b00011000);
+  OLED_write_d(0b00011000);
+  OLED_write_d(0b01111110);
+  OLED_write_d(0b00111100);
+  OLED_write_d(0b00011000);
 }
