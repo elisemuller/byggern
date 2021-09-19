ccb32153733aa44d105f2418b6edb72b96cae923
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-19 16:56:29 +0200

    Update OLED_driver.c

--- OLED_driver.c
+++ OLED_driver.c
@@ -10,3 +10,11 @@
 #include "OLED_driver.h"
 #include "xmem.h"
 
+void OLED_print_arrow(uint8_t row, uint8_t col){
+  OLED_pos(row, col); 
+  OLED_write_data(0b00011000);
+  OLED_write_data(0b00011000);
+  OLED_write_data(0b01111110);
+  OLED_write_data(0b00111100);
+  OLED_write_data(0b00011000);
+}
