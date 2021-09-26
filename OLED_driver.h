2006ffe42e00850ba5369cc96a194f1e3f566e31
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-26 18:49:49 +0200

    Added font enum

--- OLED_driver.h
+++ OLED_driver.h
@@ -9,22 +9,40 @@
 #ifndef OLED_DRIVER_H_
 #define OLED_DRIVER_H_
 
+#define OLED_SCREEN_WIDTH 128
+#define OLED_SCREEN_HEIGHT 64
+
+#define OFFSET_OLED 0x0000
+
+#define OFFSET_OLED_DATA 0x1200
+#define OFFSET_OLED_CMD 0x1000
+
+#define NUM_PAGES 8
+
+typedef enum {
+  LARGE,
+  NORMAL, 
+  SMALL
+} font;
+
 
 void OLED_init(void);
+
+void OLED_write_d(volatile char data); //volatile
+
+void OLED_write_c(volatile char command);
 
 void OLED_reset(void);
 
 void OLED_home(void);
 
-void OLED_goto_line(uint8_t line);
+void OLED_goto_column(uint8_t col); 
 
-void OLED_clear_line(uint8_t line);
+void OLED_goto_page(uint8_t page);
 
-void OLED_pos(uint8_t row, uint8_t col);
+void OLED_clear_page(uint8_t page);
 
-void OLED_write_d(volatile char data); //volatile
-
-void OLED_write_c(volatile char command);
+void OLED_goto_pos(uint8_t row, uint8_t col);
 
 void OLED_print(char* data);
 
