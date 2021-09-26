4196eb6422efb44c0919aef02ac6ece010913780
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-09-26 15:47:14 +0200

    An attempt on implementing OLED functions

--- OLED_driver.c
+++ OLED_driver.c
@@ -19,6 +19,10 @@
 #define OFFSET_OLED_DATA 0x1200
 #define OFFSET_OLED_CMD 0x1000
 
+#define NUM_PAGES 8
+#define PAGE_MODE 0x02
+#define HORIZONTAL_MODE 0x00
+
 
 
 void OLED_init(void){
@@ -34,10 +38,9 @@ void OLED_init(void){
  OLED_write_c(0x81); //contrast control
  OLED_write_c(0x50);
  OLED_write_c(0xd9); //set pre-charge period
- OLED_write_c(0x21);
+ OLED_write_c(0x21); //Set column address (only for horizontal/vertical addressing mode)? 
  OLED_write_c(0x20); //Set Memory Addressing Mode
- //OLED_write_c(0x00); //Page Addressing Mode
- //OLED_write_c(0x01); //Vertical Addressing Mode
+ OLED_write_c(PAGE_MODE); //Page Addressing Mode
  OLED_write_c(0xdb); //VCOM deselect level mode
  OLED_write_c(0x30); 
  OLED_write_c(0xad); //master configuration
@@ -45,8 +48,12 @@ void OLED_init(void){
  OLED_write_c(0xa4); //out follows RAM content
  OLED_write_c(0xa6); //set normal display
  OLED_write_c(0xaf); // display on
- 
- //implement reset function
+	
+ OLED_write_c(0xB0); // Page 0 (Address pointer points here) 
+ OLED_write_c(0x00); // (lower column address) 
+ OLED_write_c(0x10); // Starting column is SEG0 (higher column address) 
+
+ OLED_reset(); 
 }
 
 
@@ -59,8 +66,55 @@ void OLED_write_c(volatile char command){
 	xmem_write(command, OFFSET_OLED_CMD);
 }
 
+void OLED_reset(void){
+	OLED_home();
+	for (pg = 0; pg < NUM_PAGES < pg++){
+		for(col = 0; col < OLED_SCREEN_WIDTH; col++){
+			OLED_write_d(0); 
+		}
+	}
+	OLED_home();
+}
+
+void OLED_home(void){
+	OLED_goto_page(0); 
+	OLED_goto_column(0);
+}
+
+void OLED_goto_column(uint8_t col){
+	uint8_t lower_col_addr = (0x01 & col); 
+	uint8_t upper_col_addr = (0x10 & col); 
+	upper_col_addr = (0x10 + (upper_col_addr >> 4)); 
+	OLED_write_c(lower_col_addr); 
+	OLED_write_c(upper_col_addr); 
+}
+
+void OLED_goto_page(uint8_t page){ //page og line er synonymer
+	OLED_write_c(0xB0 + page);
+}
+
+void OLED_clear_page(uint8_t page){
+	OLED_goto_page(page)
+	for(col = 0; col < OLED_SCREEN_WIDTH; col++){
+		OLED_write_d(0);
+	}
+}
+
+//void OLED_write_to_pos(uint8_t row, uint8_t col, uint8_t set){
+	//uint8_t page = row / NUM_PAGES; 
+	//uint8_t page_line = row % NUM_PAGES; 
+	//OLED_goto_page(page); 
+	//OLED_goto_column(col); 
+	//OLED_write_c(set << page_line); 	
+//}
+
+void OLED_goto_pos(uint8_t row, uint8_t col){
+	OLED_goto_page(row);
+	OLED_goto_page(col); 
+}
+
 void OLED_print_arrow(uint8_t row, uint8_t col){
-  OLED_pos(row, col); 
+  OLED_goto_pos(row, col); 
   OLED_write_d(0b00011000);
   OLED_write_d(0b00011000);
   OLED_write_d(0b01111110);
