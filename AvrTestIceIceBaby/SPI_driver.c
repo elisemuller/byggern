b4256a69b3e1f68bba0b54a45dedd3decedbc40c
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-03 20:44:33 +0200

    Create SPI_driver.c

---
+++ AvrTestIceIceBaby/SPI_driver.c
@@ -0,0 +1,18 @@
+#include "SPI_driver.h"
+
+void SPI_MasterInit(void){
+  // Set MOSI, SS and SCK output, all others input
+  DDRB = (1 << PB5) | (1 << PB4) | (1 << PB7);
+  DDRB &= ~(1 << PB6); // PB6 = 0, MISO is input
+  // Enable SPI, Master, set clock rate fck/16
+  SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+  SPCR &= ~(1 << SPR1); // SPR1 = 0
+}
+
+void SPI_MasterTransmit(char cData){
+  //Start transmission
+  SPDR = cData; //SPI Data register
+  //Wait for transmission complete
+  while(!(SPSR & (1 << SPIF)){ //SPIF sets interrupt flag IF when transmission is completed
+  };
+}
