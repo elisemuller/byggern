d606e83bac28650a6d3217257bbb4e4fbc1bb0aa
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-03 21:15:15 +0200

    Update SPI_driver.h

--- AvrTestIceIceBaby/SPI_driver.h
+++ AvrTestIceIceBaby/SPI_driver.h
@@ -11,11 +11,10 @@ void SPI_init(void);
 
 //Henter fra Atmega s.160
 void SPI_MasterInit(void);
-void SPI_MasterTransmit(char cData);
+void SPI_send(char cData);
 
 //To read something from the slave the master must transmit a dummy byte
-uint8_t SPI_read(void);
+char SPI_read(void);
 
-void SPI_send(uint8_t);
 
 #endif /* SPI_DRIVER_H_ */
