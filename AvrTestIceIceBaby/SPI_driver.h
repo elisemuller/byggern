58eb0162d78378dfc658bfe06cbf98f155631ac8
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-03 21:21:20 +0200

    Update SPI_driver.h

--- AvrTestIceIceBaby/SPI_driver.h
+++ AvrTestIceIceBaby/SPI_driver.h
@@ -16,5 +16,18 @@ void SPI_send(char cData);
 //To read something from the slave the master must transmit a dummy byte
 char SPI_read(void);
 
+/**
+* @brief Selects CAN-controller
+* @param void
+* @return void                                                                     
+*/
+void SPI_select(void);
+
+/**
+* @brief Deselects CAN-controller
+* @param void
+* @return void                                                                     
+*/
+void SPI_deselect(void);
 
 #endif /* SPI_DRIVER_H_ */
