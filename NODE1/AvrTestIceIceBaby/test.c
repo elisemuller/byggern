446df3ef49e7837985c6054bdf7a3efa169258af
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-16 20:41:42 +0200

    Update test.c

--- NODE1/AvrTestIceIceBaby/test.c
+++ NODE1/AvrTestIceIceBaby/test.c
@@ -26,6 +26,30 @@ void test_address_decoder(void) {
 	_delay_ms(250);
 }
 
+
+void test_CAN_loopback(void){
+	can_message test_message;
+	test_message.id = 0x76;
+	test_message.length = 1;
+	test_message.data[0] = 'U';
+	
+	can_message test_message_2;
+	test_message_2.id = 0x7B;
+	test_message_2.length = 1;
+	test_message_2.data[0] = 'M';
+	
+	can_message test_1_received;
+	can_message test_2_received;
+	
+	while (1)
+	{
+		CAN_send_message(&test_message);
+		CAN_receive_message(&test_1_received);
+		CAN_send_message(&test_message_2);
+		CAN_receive_message(&test_2_received);
+	}
+}
+
 //mcp2515_write(0x1F, 0x02);
 
 //SPI_select();
