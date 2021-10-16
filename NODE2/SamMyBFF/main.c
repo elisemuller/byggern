ce7eebff5505d46b04f4d3fe5f3d79c3f4f19359
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-16 21:22:07 +0200

    Update main.c

--- NODE2/SamMyBFF/main.c
+++ NODE2/SamMyBFF/main.c
@@ -1,1 +1,15 @@
 
+#define CAN_BR
+
+#include "can_controller.h"
+#include "sam.h"
+#include "printf-stdarg.h"
+#include "uart.h"
+
+
+int main(void){
+  configure_uart();
+  can_init_def_tx_rx_mb(CAN_BR);
+  
+  //test can reception for joystick
+}
