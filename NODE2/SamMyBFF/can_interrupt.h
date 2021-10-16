a17b1b1ce6a85d1e5926dffabf1f94bfaad6d434
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-16 17:44:01 +0200

    Update can_interrupt.h

--- NODE2/SamMyBFF/can_interrupt.h
+++ NODE2/SamMyBFF/can_interrupt.h
@@ -1,7 +1,7 @@
 /*
  * can_interrupt.h
  *
- * Author: Gustav O. Often and Eivind H. Jølsgard
+ * Author: Gustav O. Often and Eivind H. JÃ¸lsgard
  *
  * For use in TTK4155 Embedded and Industrial Computer Systems Design
  * NTNU - Norwegian University of Science and Technology
@@ -11,6 +11,34 @@
 
 #ifndef CAN_INTERRUPT_H_
 #define CAN_INTERRUPT_H_
+
+#define CAN_JOYSTICK_ID 1
+#define CAN_SLIDER_ID 2
+
+typedef enum {
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	NEUTRAL,
+	UNDEFINED
+} dir;
+
+typedef struct input_joystick {
+	int pos_x;
+	int pos_y;
+  int button_pressed;
+  dir direction; 
+} input_j;
+
+typedef struct input_slider {
+	int pos_r_slider;
+	int pos_l_slider;
+  int r_button_pressed;
+  int l_button_pressed;
+} input_s;
+
+
 void CAN0_Handler       ( void );
 
 
