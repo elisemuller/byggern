c54bd8054096b1abdab0b16ba29240fb44e80e9a
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-22 19:44:45 +0200

    Update adc_driver.c

--- NODE2/SamMyBFF/adc_driver.c
+++ NODE2/SamMyBFF/adc_driver.c
@@ -2,25 +2,25 @@
 #include "sam.h"
 
 void adc_init(void){
-	// Disable PIO, select peripheral
-	PIOC->PIO_PDR |= PIO_PDR_P18;// Velge riktig pin her
-	PIOC->PIO_ABSR |= PIO_ABSR_P18;// 
-	
 	// Peripheral clock enable
 	// PMC->PMC_PCR |= PMC_PCR_EN | (0 << ADC_PCR_DIV_Pos) | (ID_ADC << PMC_PCR_PID_Pos); // - unødvendig?
 	PMC->PMC_PCER1 |= ( PMC_PCER1_PID37 ); //ADC ID = 37
 	
-	// Enable channel
-	// ADC_CHER |= (1 << CHx);, der x er tallet på kanalen vi vil enable
-	
 	// Freerun mode, og sover mellom conversions. Kan eventuelt sette opp fast wake up? 
-	// ADC_MR |= (1 << ADC_MR_FREERUN) | (1 << ADC_MR_SLEEP); 
+	// ADC -> ADC_MR |= (1 << ADC_MR_FREERUN) | (1 << ADC_MR_SLEEP); //Bare gjetter på hva ting heter
+	
+	// Enable channel
+	// ADC -> ADC_CHER |= ADC_CHER_CHx;, der x er tallet på kanalen vi vil enable
+	
+
   }
 
-volatile uint8_t adc_rd(uint8_t channel){
-	//Begin analog-to-digital conversion
-	ADC_CR = (0b10); // START = 1, SWRST = 0
-	uint8_t data;
-	// data = ADC_CDRx; // x = valgt kanal
+volatile uint32_t adc_rd(void){
+	// Begin analog-to-digital conversion
+	ADC -> ADC_CR |= (0b10); // START = 1, SWRST = 0
+
+	// Read converted data 
+	unit32_t data = ADC -> ADC_CDR[x]; // x = valgt kanal
+	return data;
 	
 }
