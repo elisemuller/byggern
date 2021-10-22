5f28fa7772cc90f14502193add193203a874f3da
Emilia O'Brien <73793083+Art3miss233@users.noreply.github.com>
2021-10-22 12:09:29 +0200

    Update adc_driver.c

--- NODE2/SamMyBFF/adc_driver.c
+++ NODE2/SamMyBFF/adc_driver.c
@@ -3,10 +3,24 @@
 
 void adc_init(void){
 	// Disable PIO, select peripheral
-	PIOC->PIO_PDR |= PIO_PDR_P19;// | PIO_PDR_P18;
-	PIOC->PIO_ABSR |= PIO_ABSR_P19;// | PIO_ABSR_P18;
+	PIOC->PIO_PDR |= PIO_PDR_P18;// Velge riktig pin her
+	PIOC->PIO_ABSR |= PIO_ABSR_P18;// 
 	
 	// Peripheral clock enable
 	// PMC->PMC_PCR |= PMC_PCR_EN | (0 << ADC_PCR_DIV_Pos) | (ID_ADC << PMC_PCR_PID_Pos); // - unødvendig?
 	PMC->PMC_PCER1 |= ( PMC_PCER1_PID37 ); //ADC ID = 37
+	
+	// Enable channel
+	// ADC_CHER |= (1 << CHx);, der x er tallet på kanalen vi vil enable
+	
+	// Freerun mode, og sover mellom conversions. Kan eventuelt sette opp fast wake up? 
+	// ADC_MR |= (1 << ADC_MR_FREERUN) | (1 << ADC_MR_SLEEP); 
   }
+
+volatile uint8_t adc_rd(uint8_t channel){
+	//Begin analog-to-digital conversion
+	ADC_CR = (0b10); // START = 1, SWRST = 0
+	uint8_t data;
+	// data = ADC_CDRx; // x = valgt kanal
+	
+}
