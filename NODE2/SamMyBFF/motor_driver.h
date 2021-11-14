473f34738b5b3e08b597b0369a39f666d170d599
elisemuller <86289945+elisemuller@users.noreply.github.com>
2021-11-14 16:59:58 +0100

    Update motor_driver.h

--- NODE2/SamMyBFF/motor_driver.h
+++ NODE2/SamMyBFF/motor_driver.h
@@ -12,11 +12,15 @@
 #include <stdint.h>
 #include "can_interrupt.h"
 
+/**
+ * @brief 
+ */
 void motor_init(void);
 
+/**
+ * @brief 
+ */
 uint16_t motor_read_encoder(void);
-
-
 
 /**
  * @brief Sets the speed of the motor 
@@ -24,14 +28,30 @@ uint16_t motor_read_encoder(void);
  */
 void motor_set_speed(uint32_t speed);
 
+/**
+ * @brief 
+ */
 void motor_power(int on);
 
+/**
+ * @brief Sets the direction of the motor
+ * @param direction RIGHT or LEFT
+ */
 void motor_set_direction(dir direction);
 
+/**
+ * @brief 
+ */
 void motor_controller(void);
 
+/**
+ * @brief 
+ */
 void motor_reset_encoder(void);
 
+/**
+ * @brief 
+ */
 void motor_calibrate_encoder(void);
 
 
