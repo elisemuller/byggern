b0329f77030799a2be582ccf00e0b2fbf1f9ee2a
elisemuller <86289945+elisemuller@users.noreply.github.com>
2021-11-14 16:33:36 +0100

    Update blink.h

--- NODE2/SamMyBFF/blink.h
+++ NODE2/SamMyBFF/blink.h
@@ -9,8 +9,15 @@
 #ifndef BLINK_H_
 #define BLINK_H_
 
+
+/** 
+* @brief Initializes the LED-pins. 
+**/
 void led_enable(void);
 
+/**
+* @brief Makes the LEDs on the SAM blink alternatingly
+**/
 void blink(void);
 
 #endif /* BLINK_H_ */
