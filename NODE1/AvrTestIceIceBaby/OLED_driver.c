49604302fcfd6c41c20f788415b04e6c45c4f497
Elise Gulløy Müller <elise.gulloy@gmail.com>
2021-11-19 09:41:58 +0100

    remove graphics

--- NODE1/AvrTestIceIceBaby/OLED_driver.c
+++ NODE1/AvrTestIceIceBaby/OLED_driver.c
@@ -3,7 +3,7 @@
  *
  * Created: 06.09.2021 15:26:19
  *  Author: elisegm
- */ 
+ */
 
 #include <avr/io.h>
 #include <stdio.h>
@@ -12,10 +12,9 @@
 #include "OLED_driver.h"
 #include "xmem.h"
 #include "fonts.h"
-#include "Graphics.h"
 
 
-volatile font style;  
+volatile font style;
 volatile int current_page = 0;
 
 
@@ -32,22 +31,22 @@ void OLED_init(void){
  OLED_write_c(0x81); //contrast control
  OLED_write_c(0x50);
  OLED_write_c(0xd9); //set pre-charge period
- OLED_write_c(0x21); //Set column address (only for horizontal/vertical addressing mode)? 
+ OLED_write_c(0x21); //Set column address (only for horizontal/vertical addressing mode)?
  OLED_write_c(0x20); //Set Memory Addressing Mode
  OLED_write_c(0x02); //Page Addressing Mode
  OLED_write_c(0xdb); //VCOM deselect level mode
- OLED_write_c(0x30); 
+ OLED_write_c(0x30);
  OLED_write_c(0xad); //master configuration
  OLED_write_c(0x00);
  OLED_write_c(0xa4); //out follows RAM content
  OLED_write_c(0xa6); //set normal display
  OLED_write_c(0xaf); // display on
-	
- OLED_write_c(0xB0); // Page 0 (Address pointer points here) 
- OLED_write_c(0x00); // (lower column address) 
- OLED_write_c(0x10); // Starting column is SEG0 (higher column address) 
 
- OLED_reset(); 
+ OLED_write_c(0xB0); // Page 0 (Address pointer points here)
+ OLED_write_c(0x00); // (lower column address)
+ OLED_write_c(0x10); // Starting column is SEG0 (higher column address)
+
+ OLED_reset();
  OLED_select_font(LARGE);
 }
 
@@ -70,22 +69,22 @@ void OLED_reset(void){
 }
 
 void OLED_home(void){
-	OLED_goto_page(0); 
+	OLED_goto_page(0);
 	OLED_goto_column(0);
-	current_page = 0; 
+	current_page = 0;
 }
 
 void OLED_goto_column(uint8_t col){
-	uint8_t lower_col_addr = (0x01 & col); 
-	uint8_t upper_col_addr = (0x10 & col); 
-	upper_col_addr = (0x10 + (upper_col_addr >> 4)); 
-	OLED_write_c(lower_col_addr); 
-	OLED_write_c(upper_col_addr); 
+	uint8_t lower_col_addr = (0x01 & col);
+	uint8_t upper_col_addr = (0x10 & col);
+	upper_col_addr = (0x10 + (upper_col_addr >> 4));
+	OLED_write_c(lower_col_addr);
+	OLED_write_c(upper_col_addr);
 }
 
-void OLED_goto_page(uint8_t page){ 
+void OLED_goto_page(uint8_t page){
 	OLED_write_c(0xB0 + page);
-	current_page = page; 
+	current_page = page;
 }
 
 void OLED_clear_page(uint8_t page){
@@ -98,30 +97,30 @@ void OLED_clear_page(uint8_t page){
 
 void OLED_goto_pos(uint8_t row, uint8_t col){
 	OLED_goto_page(row);
-	OLED_goto_column(col); 
+	OLED_goto_column(col);
 }
 
 void OLED_select_font(font format){
-	style = format; 
+	style = format;
 }
 
 void OLED_write_char(uint8_t ch){
 	switch(style){
 		case SMALL:
 			for(uint8_t data = 0; data < 4; data ++){
-				uint8_t byte = pgm_read_byte(&(font4[ch-32][data])); 
+				uint8_t byte = pgm_read_byte(&(font4[ch-32][data]));
 				OLED_write_d(byte);
 			}
-			break; 
+			break;
 		case MEDIUM:
 			for(uint8_t data = 0; data < 5; data ++){
-				uint8_t byte = pgm_read_byte(&(font5[ch-32][data])); 
+				uint8_t byte = pgm_read_byte(&(font5[ch-32][data]));
 				OLED_write_d(byte);
-			}			
-			break; 
-		case LARGE: 
+			}
+			break;
+		case LARGE:
 			for(uint8_t data = 0; data < 8; data ++){
-				uint8_t byte = pgm_read_byte(&(font8[ch-32][data])); 
+				uint8_t byte = pgm_read_byte(&(font8[ch-32][data]));
 				OLED_write_d(byte);
 			}
 
@@ -137,11 +136,11 @@ void OLED_print(char* data){
 		if(size_ch*8 < OLED_SCREEN_WIDTH){
 			OLED_write_char(data[ch]);
 			size_ch++;
-			ch++;		
+			ch++;
 		}
 		else{
 			OLED_goto_page(current_page + 1);
-			size_ch = 0; 
+			size_ch = 0;
 		}
 
 	}
@@ -149,7 +148,7 @@ void OLED_print(char* data){
 
 
 void OLED_print_arrow(uint8_t row, uint8_t col){
-  OLED_goto_pos(row, col); 
+  OLED_goto_pos(row, col);
   OLED_write_d(0b00011000);
   OLED_write_d(0b00011000);
   OLED_write_d(0b01111110);
